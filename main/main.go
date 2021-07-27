427ab60b27425fec6bbae07ab723d5b1ac541a67
nika <kjkjttff@gmail.com>
2021-07-27 16:26:31 +0300

    commit before refactoring

--- main/main.go
+++ main/main.go
@@ -1,7 +1,11 @@
 package main
 
 import (
+	"bytes"
 	"errors"
+	"regexp"
+	"strings"
+
 	//"errors"
 	"fmt"
 	"net/http"
@@ -11,27 +15,27 @@ import (
 
 type throttler struct {
 	http.RoundTripper
-	limit       int
-	interval    time.Duration
-	methods     []string
-	urlPrefixes []string
-	exceptions  []string
-	fastReturn  bool
-	queue       Queue
-	count       int
-	mux         sync.Mutex
+	limit           int
+	interval        time.Duration
+	methods         []string
+	allowedPrefixes []string
+	exceptions      []string
+	fastReturn      bool
+	queue           Queue
+	count           int
+	mux             sync.Mutex
 }
 
 func NewThrottler(roundTripper http.RoundTripper, limit int, interval time.Duration, methods, urlPrefixes, exceptions []string, fastReturn bool) *throttler {
 	t := &throttler{
-		RoundTripper: roundTripper,
-		limit:        limit,
-		interval:     interval,
-		methods:      methods,
-		urlPrefixes:  urlPrefixes,
-		exceptions:   exceptions,
-		fastReturn:   fastReturn,
-		queue:        *NewQueue(limit),
+		RoundTripper:    roundTripper,
+		limit:           limit,
+		interval:        interval,
+		methods:         methods,
+		allowedPrefixes: getRegex(urlPrefixes),
+		exceptions:      getRegex(exceptions),
+		fastReturn:      fastReturn,
+		queue:           *NewQueue(limit),
 	}
 
 	go t.run()
@@ -109,6 +113,34 @@ func (t *throttler) releaseRequests() {
 //		return t.RoundTripper.RoundTrip(req)
 //	}
 //}
+func getRegex(strs []string) []string {
+	result := make([]string, len(strs))
+
+	for i, str := range strs {
+		if len(str) == 0 {
+			continue
+		}
+
+		var strsSplited []string
+		var regex bytes.Buffer
+
+		strsSplited = append(strsSplited, strings.Split(str, "*")...)
+		regex.WriteString("^")
+
+		for i, expr := range strsSplited {
+			regex.WriteString(expr)
+
+			if strsSplited[i] == "" || i+1 < len(strsSplited) && strsSplited[i+1] != "" {
+				regex.WriteString(".*")
+			}
+		}
+
+		regex.WriteString("$")
+		result[i] = regex.String()
+	}
+
+	return result
+}
 
 type Decorated http.RoundTripper
 type RoundTripFunc func(*http.Request) (*http.Response, error) // this is the type of functions you want to decorate
@@ -129,9 +161,31 @@ func (r RoundTripFunc) RoundTrip(req *http.Request) (*http.Response, error) {
 func AppendDecorator(t *throttler) Decorator {
 	return func(tripper http.RoundTripper) http.RoundTripper {
 		return RoundTripFunc(func(req *http.Request) (resp *http.Response, err error) {
+			fmt.Println(req.URL.Path)
 			if t.limit == 0 { //если не удовлетворяет условиям //проверка на префиксы и методы
 				return t.RoundTripper.RoundTrip(req)
 			}
+
+			for _, exception := range t.exceptions {
+				match, err := regexp.MatchString(exception, req.URL.String())
+				if err != nil {
+					return resp, err
+				}
+				if match {
+					return t.RoundTripper.RoundTrip(req)
+				}
+			}
+
+			for _, urlPrefix := range t.allowedPrefixes {
+				match, err := regexp.MatchString(urlPrefix, req.URL.String())
+				if err != nil {
+					return resp, err
+				}
+				if !match {
+					return t.RoundTripper.RoundTrip(req)
+				}
+			}
+
 			if len(t.methods) != 0 {
 				shouldReturn := true
 				for _, m := range t.methods {
@@ -174,8 +228,8 @@ func main() {
 		time.Second*10,                    // 60 rpm
 		[]string{"POST", "PUT", "DELETE"}, // limit only POST, PUT, DELETE requests
 		nil,                               // use for all URLs
-		[]string{"/servers/*/status", "/network/"}, // except servers status and network operations
-		false, // wait on limit
+		[]string{},                        // except servers status and network operations
+		false,                             // wait on limit
 	)
 
 	var roundTrip Decorator = AppendDecorator(throttler)
@@ -186,17 +240,14 @@ func main() {
 		Transport: throttled,
 	}
 
-	// ...
-	for i := 0; i < 12; i++ {
-		go func() {
-			req, _ := http.NewRequest("PUT", "http://apidomain.com/images/reload", nil)
-			_, _ = client.Do(req)
-		}()
-	}
-	_, _ = client.Get("http://apidomain.com/network/routes") // no throttling
-	// ...
-	req, _ := http.NewRequest("PUT", "http://apidomain.com/images/reload", nil)
+	_, _ = client.Get("http://apidomain.com/servers/1337/status?simple=true")
+	//_, _ = client.Do(req)
+
+	req, _ := http.NewRequest("PUT", "http://apidomain.com/network/", nil)
+	_, _ = client.Do(req) // throttling might be used
+
+	req, _ = http.NewRequest("PUT", "http://apidomain.com/servers/153/status", nil)
 	_, _ = client.Do(req) // throttling might be used
 	// ...
-	_, _ = client.Get("http://apidomain.com/servers/1337/status?simple=true") // no throttling
+
 }
